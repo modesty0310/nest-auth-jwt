828487574a681a42b6b997bdc6b2eb3af35ecdd8
modesty0310 <rlatprua4@gmail.com>
2023-01-05 14:41:13 +0900

    회원가입 기능 예외처리

--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -8,9 +8,7 @@ export class UserController {
     constructor(private readonly userService: UserService){}
 
     @Post()
-    create(@Body() createUserDto: CreateUserDto): Promise<User> {
-        console.log(createUserDto);
-        
+    create(@Body() createUserDto: CreateUserDto): Promise<User> {        
         return this.userService.create(createUserDto)
     }
 }
--- src/users/user.service.ts
+++ src/users/user.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
+import * as bcrypt from "bcrypt";
 import { CreateUserDto } from "./dto/create-user.dto";
 import { User } from "./user.entity";
 
@@ -12,9 +13,10 @@ export class UserService {
     ){}
     async create(createUserDto: CreateUserDto):Promise<User> {
         const {email, password} = createUserDto;
+        const hashedPassword = await bcrypt.hash(password, 10);
         const result = await this.userRepository.save({
             email,
-            password
+            password: hashedPassword
         });
         return result
     }
