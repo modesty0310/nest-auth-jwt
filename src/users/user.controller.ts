a74a8390bd39b3cb10cecab86f34a09ddc52c9f2
modesty0310 <rlatprua4@gmail.com>
2023-01-06 13:05:06 +0900

    login

--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Post } from "@nestjs/common";
 import { CreateUserDto } from "./dto/create-user.dto";
+import { LoginUserDto } from "./dto/login-uiser.dto";
 import { User } from "./user.entity";
 import { UserService } from "./user.service";
 
@@ -10,5 +11,9 @@ export class UserController {
     @Post()
     create(@Body() createUserDto: CreateUserDto): Promise<User> {        
         return this.userService.create(createUserDto)
+    }
+    @Post("login")
+    login(@Body() loginUserDto: LoginUserDto) {
+        this.userService.login(loginUserDto);
     }
 }
--- src/users/user.service.ts
+++ src/users/user.service.ts
@@ -1,9 +1,10 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, UnauthorizedException } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
 import * as bcrypt from "bcrypt";
 import { CreateUserDto } from "./dto/create-user.dto";
 import { User } from "./user.entity";
+import { LoginUserDto } from "./dto/login-uiser.dto";
 
 @Injectable()
 export class UserService {
@@ -11,6 +12,7 @@ export class UserService {
         @InjectRepository(User)
         private readonly userRepository: Repository<User>
     ){}
+
     async create(createUserDto: CreateUserDto):Promise<User> {
         const {email, password} = createUserDto;
         const hashedPassword = await bcrypt.hash(password, 10);
@@ -19,5 +21,22 @@ export class UserService {
             password: hashedPassword
         });
         return result
+    }
+
+    async login(loginUserDto: LoginUserDto) {
+        const {email, password} = loginUserDto;
+        const user = await this.findOneUser(email);
+        
+        if(!user) throw new UnauthorizedException("이메일과 비밀번호를 확인해 주세요.");
+
+        const isPasswordCompare = await bcrypt.compare(password, user.password);
+
+        if(!isPasswordCompare) throw new UnauthorizedException("이메일과 비밀번호를 확인해 주세요");
+
+        const payload = { email: email, sub: user.id };
+    }
+
+    async findOneUser(email: string): Promise<User> | null {
+        return await this.userRepository.findOne({where: {email}});
     }
 }
